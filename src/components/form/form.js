3b783297827f8e57bda9cedfc010941135c46b5e
Viacheslav <brightsdayss@gmail.com>
2020-12-19 20:16:56 +0300

    set storage, fix styles

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -5,19 +5,31 @@ import Results from './results';
 import Calc from './calc';
 
 const Form = () => {
-  const [price, setPrice] = useState('');
-  const [part, setPart] = useState('');
-  const [time, setTime] = useState('');
-  const [percent, setPercent] = useState('');
+  const savedPrice = localStorage.getItem('price');
+  const savedPart = localStorage.getItem('part');
+  const savedTime = localStorage.getItem('time');
+  const savedPercent = localStorage.getItem('percent');
+
+  const [price, setPrice] = useState(savedPrice || '0');
+  const [part, setPart] = useState(savedPart || '0');
+  const [time, setTime] = useState(savedTime || '0');
+  const [percent, setPercent] = useState(savedPercent || '0');
   const [data, setData] = useState({
     loanBody: 0,
     monthlyPayment: 0,
     requiredIncome: 0,
     overpayment: 0,
   });
-  //const [disabled, setDisabled] = useState(false);
+  const [disabled, setDisabled] = useState(false);
 
   useEffect(() => setData(Calc(price, part, time, percent)), [price, part, time, percent]);
+
+  const saveData = () => {
+    localStorage.setItem('price', price);
+    localStorage.setItem('part', part);
+    localStorage.setItem('time', time);
+    localStorage.setItem('percent', percent);
+  };
 
   const Clean = (e) => {
     e.preventDefault();
@@ -32,18 +44,15 @@ const Form = () => {
       requiredIncome: 0,
       overpayment: 0,
     });
-    //setDisabled(false);
+    setDisabled(false);
     document.querySelectorAll('input').forEach(item => item.value = '');
     document.querySelectorAll('input[name="anchor"]').forEach(item => item.checked = false);
   };
 
   const anchor = (value) => {
     const nPrice = price.replace(/\s/g, "");
-    //setDisabled(true);
+    setDisabled(true);
     setPart(Math.round(+nPrice * value / 100).toString());
-    document.querySelector('.val').value = Math.round(+nPrice * value / 100)
-                                               .toString()
-                                               .replace(/\B(?=(\d{3})+(?!\d))/g, " ");
   };
 
   return (
@@ -54,7 +63,8 @@ const Form = () => {
           <input onInput={e => {
                             Validate(e.target, 10000000000);
                             setPrice(e.target.value);}} 
-                 placeholder="До 10 000 000 000"></input>
+                 placeholder="До 10 000 000 000" 
+                 value={price}></input>
         </div>
         <div className="mortgage-form__input">
           <label>Первоначальный взнос</label>
@@ -62,7 +72,8 @@ const Form = () => {
                             Validate(e.target, 3300000000);
                             setPart(e.target.value);}} 
                  placeholder="До 3 300 000 000"
-                 /*disabled={disabled}*/></input>
+                 value={part}
+                 disabled={disabled}></input>
         </div>
         <div className="mortgage-form__anchors">
           <input type="radio" name="anchor" value="10" onChange={e => anchor(e.target.value)}/>
@@ -76,17 +87,19 @@ const Form = () => {
           <input onInput={e => {
                             Validate(e.target, 30);
                             setTime(e.target.value);}} 
-                 placeholder="До 30"></input>
+                 placeholder="До 30"
+                 value={time}></input>
         </div>
         <div className="mortgage-form__input">
           <label>Процентная ставка</label>
           <input onInput={e => {
                             Validate(e.target, 30);
                             setPercent(e.target.value);}} 
-                 placeholder="До 30"></input>
+                 placeholder="До 30"
+                 value={percent}></input>
         </div>
         <div className="mortgage-form__buttons">
-          <button onClick={(e) => {e.preventDefault();console.log(data)}}>Сохранить</button>
+          <button onClick={(e) => {e.preventDefault();saveData()}}>Сохранить</button>
           <button onClick={e => Clean(e)}>Очистить</button>
         </div>
       </form>
