4f3a2eeb5d594a080e0e354444690a8ca45de181
Viacheslav Ivanov <brightsdayss@gmail.com>
2021-07-06 15:35:15 +0300

    Updates

---
+++ precache-manifest.9d95fa9ffca765c4775d2312c1ecf3f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b57f473a79dc2ad426bf88b7ca46bc3",
+    "url": "/mortgage-calculator/index.html"
+  },
+  {
+    "revision": "44f11c1602783adfd39d",
+    "url": "/mortgage-calculator/static/css/main.a1f72990.chunk.css"
+  },
+  {
+    "revision": "37879b2aab5e2f49d31c",
+    "url": "/mortgage-calculator/static/js/2.fb27523d.chunk.js"
+  },
+  {
+    "revision": "44f11c1602783adfd39d",
+    "url": "/mortgage-calculator/static/js/main.19980049.chunk.js"
+  },
+  {
+    "revision": "403991d954b72fee3317",
+    "url": "/mortgage-calculator/static/js/runtime~main.68e02ac6.js"
+  }
+]);
